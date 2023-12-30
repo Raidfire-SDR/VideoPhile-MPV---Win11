d8f5eab85e1905511f0a5b7d29f9891d530a9656
Raidfire-SDR <125891383+Raidfire-SDR@users.noreply.github.com>
2023-12-30 22:34:31 +0200

    Add files via upload

---
+++ HDR Switch.bat
@@ -0,0 +1,18 @@
+Echo Welcome To HDR / SDR Switch
+Echo Press 1 for SDR 2 for HDR
+Echo Off
+cd C:\Users\raidf\Documents\mpv
+del mpv.conf
+set /p input=Enter some text: 
+If /I %input% == 1 goto SDR
+If /I %input% == 2 goto HDR
+
+:SDR
+type SDR_Conf >> mpv.conf
+Echo You chose SDR
+EXIT /B
+
+:HDR
+ECHO You Chose HDR
+type HDR_Conf >> mpv.conf
+EXIT /B
