b899f5f32b28f7040f9c066d32e8ba9b90045f44
Raidfire-SDR <125891383+Raidfire-SDR@users.noreply.github.com>
2023-12-31 01:14:54 +0200

    Add files via upload

--- HDR Switch.bat
+++ HDR Switch.bat
@@ -6,15 +6,33 @@ del mpv.conf.bak
 type mpv.conf >> mpv.conf.bak
 del mpv.conf
 set /p input=Enter some text: 
-If /I %input% == 1 goto SDR
-If /I %input% == 2 goto HDR
+If /I %input% == 1 goto HDR
+If /I %input% == 2 goto SDR
+If /I %input% == 3 goto YT
 
 :SDR
 type SDR_Conf >> mpv.conf
 Echo You chose SDR
+Echo Exiting in 5 seconds
+timeout /T 5
+
 EXIT /B
 
 :HDR
 ECHO You Chose HDR
 type HDR_Conf >> mpv.conf
+Echo Exiting in 5 seconds
+timeout /T 5
+
+EXIT /B
+
+:YT
+ECHO You Chose SDR with Browser Server
+type SDR_Conf >> mpv.conf
+Echo Starting server in 5 seconds
+timeout /T 5
+Echo CTRL-C or close this window to shut down server
+"MPV Browser Server.exe"
+
+
 EXIT /B
